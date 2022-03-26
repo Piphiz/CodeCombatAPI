f4f54aa30f3cdb7319ef03981c183619d07f4128
Eduardo Berg <77705253+Piphiz@users.noreply.github.com>
2022-03-26 16:26:37 -0300

    Translating Faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -12,12 +13,19 @@ class UserFactory extends Factory
      *
      * @return array
      */
+    protected $model = User::class;
+
+    public function withFaker()
+    {
+        return \Faker\Factory::create('pt_BR');
+    }
+
     public function definition()
     {
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'CPF' => $this->faker->cpf()->unique(),
+            'CPF' => $this->faker->cpf(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
     }
