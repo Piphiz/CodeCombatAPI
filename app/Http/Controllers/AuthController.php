7c39e989f195eebcfee2368b94964a7ae0663632
Mauro <maurofreirejr@gmail.com>
2022-03-26 13:17:06 -0300

    correcao jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,7 +24,6 @@ class AuthController extends Controller
     public function login()
     {
         $credentials = request(['email', 'password']);
-        dd($credentials);
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
